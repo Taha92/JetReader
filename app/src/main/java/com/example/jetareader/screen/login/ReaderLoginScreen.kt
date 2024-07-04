740bace2a126971ba788dcd9a24787e9b9e4ca8e
Taha Imtiaz <taha@inspakt.com>
2024-07-04 21:17:16 +0300

    Login and create account

--- app/src/main/java/com/example/jetareader/screen/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/example/jetareader/screen/login/ReaderLoginScreen.kt
@@ -1,26 +1,26 @@
 package com.example.jetareader.screen.login
 
-import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -30,21 +30,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.jetareader.R
 import com.example.jetareader.component.EmailInput
 import com.example.jetareader.component.PasswordInput
 import com.example.jetareader.component.ReaderLogo
 
 @Composable
 fun ReaderLoginScreen(navController: NavController) {
+    val showLoginForm = rememberSaveable { mutableStateOf(true) }
+
     Surface(modifier = Modifier
         .fillMaxSize()
     ) {
@@ -53,9 +52,34 @@ fun ReaderLoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
-            UserForm(loading = false, isCreateAccount = false) { email, password ->
-                Log.e("Form", "ReaderLoginScreen: $email $password")
+            if (showLoginForm.value) {
+                UserForm(loading = false, isCreateAccount = false) { email, password ->
+                    //Todo FB login
+                }
+            } else {
+                UserForm(loading = false, isCreateAccount = true) { email, password ->
+                    //Todo Create FB account
+                }
             }
+        }
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(modifier = Modifier
+            .padding(15.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val text = if (showLoginForm.value) "Sign Up" else "Login"
+            Text(text = "New User?")
+            Text(
+                text,
+                modifier = Modifier
+                    .clickable {
+                        showLoginForm.value = !showLoginForm.value
+                    }
+                    .padding(start = 5.dp),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
         }
     }
 }
@@ -77,16 +101,29 @@ fun UserForm(
         email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
     }
     val modifier = Modifier
-        .height(250.dp)
+        .height(260.dp)
         .background(MaterialTheme.colorScheme.background)
         .verticalScroll(rememberScrollState())
 
     Column(modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions {
-            passwordFocusRequest.requestFocus()
-        })
+        if (isCreateAccount) {
+            Text(
+                text = stringResource(id = R.string.create_acct),
+                modifier = Modifier
+                    .padding(4.dp)
+            )
+        } else {
+            Text(text = "")
+        }
+
+        EmailInput(
+            emailState = email,
+            enabled = !loading,
+            onAction = KeyboardActions {
+                passwordFocusRequest.requestFocus()
+            })
 
         PasswordInput(modifier = Modifier
             .focusRequester(passwordFocusRequest),
@@ -99,6 +136,35 @@ fun UserForm(
                 onDone(email.value.trim(), password.value.trim())
             }
         )
+
+        SubmitButton(
+            textId = if (isCreateAccount) "Create Account" else "Login",
+            loading = loading,
+            validInputs = valid
+        ) {
+            onDone(email.value.trim(), password.value.trim())
+            keyboardController?.hide()
+        }
+    }
+}
+
+@Composable
+fun SubmitButton(
+    textId: String,
+    loading: Boolean,
+    validInputs: Boolean,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        enabled = !loading && validInputs,
+        shape = CircleShape
+    ) {
+        if (loading) CircularProgressIndicator(modifier = Modifier.size(25.dp))
+            else Text(text = textId, modifier = Modifier.padding(5.dp))
     }
 }
 
