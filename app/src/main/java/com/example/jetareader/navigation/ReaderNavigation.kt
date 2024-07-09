ebc87ecb23908caf27cb9fda3eaca54f988cd1de
Taha Imtiaz <taha@inspakt.com>
2024-07-09 18:03:04 +0300

    Working on stats screen

--- app/src/main/java/com/example/jetareader/navigation/ReaderNavigation.kt
+++ app/src/main/java/com/example/jetareader/navigation/ReaderNavigation.kt
@@ -61,7 +61,8 @@ fun ReaderNavigation() {
         }
 
         composable(ReaderScreens.ReaderStatsScreen.name) {
-            ReaderStatsScreen(navController = navController)
+            val homeViewModel = hiltViewModel<HomeScreenViewModel>()
+            ReaderStatsScreen(navController = navController, viewModel = homeViewModel)
         }
     }
 }
--- app/src/main/java/com/example/jetareader/screen/stats/ReaderStatsScreen.kt
+++ app/src/main/java/com/example/jetareader/screen/stats/ReaderStatsScreen.kt
@@ -1,10 +1,122 @@
 package com.example.jetareader.screen.stats
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.sharp.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetareader.component.ReaderAppBar
+import com.example.jetareader.model.MBook
+import com.example.jetareader.navigation.ReaderScreens
+import com.example.jetareader.screen.home.HomeScreenViewModel
+import com.google.firebase.auth.FirebaseAuth
+import java.util.Locale
 
 @Composable
-fun ReaderStatsScreen(navController: NavController) {
-    Text(text = "Stats")
+fun ReaderStatsScreen(navController: NavController,
+                      viewModel: HomeScreenViewModel = hiltViewModel()
+) {
+    var books: List<MBook>
+    val currentUser = FirebaseAuth.getInstance().currentUser
+
+    Scaffold(topBar = {
+        ReaderAppBar(title = "Book Stats",
+            icon = Icons.Default.ArrowBack,
+            showProfile = false,
+            navController = navController
+        ){
+            navController.popBackStack()
+        }
+    }) {
+        Surface(modifier = Modifier
+            .padding(top = it.calculateTopPadding()
+            )
+        ) {
+            //only show books by this user that have been read
+            books = if (!viewModel.data.value.data.isNullOrEmpty()) {
+                viewModel.data.value.data!!.filter { mBook ->
+                    (mBook.userId == currentUser?.uid)
+                }
+            }else {
+                emptyList()
+            }
+
+            Column {
+                Row {
+                    Box(modifier = Modifier
+                        .size(45.dp)
+                        .padding(2.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Sharp.Person,
+                            contentDescription = "Person icon"
+                        )
+                    }
+                    
+                    Text(text = "Hi, ${
+                        currentUser?.email.toString()
+                            .split("@")[0].uppercase(Locale.getDefault())}")
+                }
+
+                Card(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp),
+                    shape = CircleShape,
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 5.dp
+                    )
+                ) {
+                    val readBooksList: List<MBook> = if (!viewModel.data.value.data.isNullOrEmpty()) {
+                        books.filter { mBook ->
+                            (mBook.userId == currentUser!!.uid) && (mBook.finishedReading != null)
+                        }
+                    } else {
+                        emptyList()
+                    }
+
+                    val readingBooks = books.filter { mBook ->
+                        (mBook.startedReading != null && mBook.finishedReading == null)
+                    }
+
+                    Column(modifier = Modifier
+                        .padding(start = 25.dp, top = 4.dp, bottom = 4.dp),
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = "Your Stats",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                        Divider()
+                        Text(text = "You're reading: ${readingBooks.size}")
+                        Text(text = "You've read: ${readBooksList.size}")
+                    }
+                }
+            }
+        }
+    }
 }
